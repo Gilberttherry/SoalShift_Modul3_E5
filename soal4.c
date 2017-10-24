7527eddf5eb6ee74b1ddb1bd4c260d8a801b577f
Gilberttherry <gilbertherry333099@gmail.com>
2017-10-24 22:10:27 +0700

    hampir bener tapi masih error gatau kenapa

--- soal4.c
+++ soal4.c
@@ -4,9 +4,31 @@
 #include<stdlib.h>
 #include<unistd.h>
 #include<stdlib.h>
-int* playAndCount(void *arg)       
-{
 
+int angka[100];
+int x;
+
+void* playAndCount(void *arg)       
+{
+	x=0;
+	int *bil = arg;
+	int hasil=1;
+	if(angka[*bil]!=0)
+	{
+		for(int i=2;i<=angka[*bil];i++)
+		{
+			hasil*=i;
+		}
+	}
+	else if(angka[*bil]==1)
+	{
+		hasil=1;
+	}
+	else
+	{
+		
+	x=1;
+	printf("Hasil %d! = %d\n",angka[*bil],hasil);
 }
 
 int main(void)
@@ -14,24 +36,38 @@ int main(void)
         int k=0;
 	int i=0;
 	int err;
+	int temp1,temp2;
 	char temp;
-    	int angka[100];
 	if(temp!='\n')
 	do
 	{ 
 	scanf("%d%c", &angka[k],&temp);
 	k++;
+	if(k!=1)
+		{
+		int n=1;
+		temp1=angka[k-n];
+		temp2=angka[k-n-1];
+		while(temp1<temp2)
+			{
+				angka[k-n]=temp2;
+				angka[k-n-1]=temp1;
+				n++;
+				temp1=angka[k-n];
+				temp2=angka[k-n-1];
+			}	
+		}
 	} while(temp!='\n');
-	k-=1;
+	pthread_t tid[k];
 	while(i<k)
     	{
-		int temp1=angka[i];
-        	err=pthread_create(&(tid[i]),NULL,&playandcount,NULL);
-		printf("Hasil %d!= %d",i,err);
-        	i++;	
+		x=0;
+        	err=pthread_create(&(tid[i]),NULL,playAndCount,(void*)&i);
+		while(x!=1){}
+		i++;	
 
     	}
-    	pthread_join(tid[0],NULL);
-    	pthread_join(tid[1],NULL);
+    	for(int a=0;a<k;a++){
+    	pthread_join(tid[a],NULL);}
     	return 0;	
 }
