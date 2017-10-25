1e0f0fb6cbb219fc53fd4deb92b8bae4f2a15171
Unknown <rtejakusuma21@yahoo.com>
2017-10-25 10:39:17 +0700

    Fungsi pembeli

--- soal1.c
+++ soal1.c
@@ -7,6 +7,7 @@
 
 char weapon[6][10] = {"MP4A1", "PM2-V1", "SPR-3", "SS2-V5", "SPG1-V3", "MINE"};
 void penjual(int* spek);
+void pembeli(int* spek);
 void menu_jual();
 void menu_beli();
 int main()
@@ -16,6 +17,71 @@ int main()
 	int shmid=shmget(key,sizeof(int), IPC_CREAT | 0666);
 	value=shmat(shmid,NULL,0);
 
+}
+void pembeli(int* spek)
+{
+	int pilih_beli;
+	char nama[100];
+	int jumlah_beli;
+	while(1)
+	{
+		menu_beli();
+		scanf("%d",&pilih_beli);
+		if(pilih_beli==1)
+		{
+			for(int i=0;i<6;i++)
+				printf("%s %d\n",weapon[i],spek[i]);
+		}
+		else if(pilih_beli==2)
+		{
+			printf("Nama_senjata jumlah\n");
+			scanf("%s %d",&nama,&jumlah_beli);
+			if(strcmp(nama,"MP4A1")==0)
+			{
+				if(spek[0]>=jumlah_beli)
+					spek[0]-=jumlah_beli;
+				else
+					printf("Maaf jumlah stok barang tidak mencukupi\n");
+			}
+			else if(strcmp(nama,"PM2-V1")==0)
+			{
+				if(spek[1]>=jumlah_beli)
+					spek[1]-=jumlah_beli;
+				else
+					printf("Maaf jumlah stok barang tidak mencukupi\n");
+			}
+			else if(strcmp(nama,"SPR-3")==0)
+			{
+				if(spek[2]>=jumlah_beli)
+					spek[2]-=jumlah_beli;
+				else
+					printf("Maaf jumlah stok barang tidak mencukupi\n");
+			}
+			else if(strcmp(nama,"SS2-V5")==0)
+			{
+				if(spek[3]>=jumlah_beli)
+					spek[3]-=jumlah_beli;
+				else
+					printf("Maaf jumlah stok barang tidak mencukupi\n");
+			}
+			else if(strcmp(nama,"SPG1-V3")==0)
+			{
+				if(spek[4]>=jumlah_beli)
+					spek[4]-=jumlah_beli;
+				else
+					printf("Maaf jumlah stok barang tidak mencukupi\n");
+			}
+			else if(strcmp(nama,"MINE")==0)
+			{
+				if(spek[5]>=jumlah_beli)
+					spek[5]-=jumlah_beli;
+				else
+					printf("Maaf jumlah stok barang tidak mencukupi\n");
+			}
+		}
+		else
+			printf("Invalid input try again bro\n");
+	}
 }
 void penjual(int* spek)
 {
@@ -36,23 +102,23 @@ void penjual(int* spek)
 			{
 				spek[0]+=jumlah;
 			}
-			if(strcmp(nama,"PM2-V1")==0)
+			else if(strcmp(nama,"PM2-V1")==0)
 			{
 				spek[1]+=jumlah;
 			}
-			if(strcmp(nama,"SPR-3")==0)
+			else if(strcmp(nama,"SPR-3")==0)
 			{
 				spek[2]+=jumlah;
 			}
-			if(strcmp(nama,"SS2-V5")==0)
+			else if(strcmp(nama,"SS2-V5")==0)
 			{
 				spek[3]+=jumlah;
 			}
-			if(strcmp(nama,"SPG1-V3")==0)
+			else if(strcmp(nama,"SPG1-V3")==0)
 			{
 				spek[4]+=jumlah;
 			}
-			if(strcmp(nama,"MINE")==0)
+			else if(strcmp(nama,"MINE")==0)
 			{
 				spek[5]+=jumlah;
 			}
