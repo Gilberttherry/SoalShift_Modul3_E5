4bc7e1a76eeb6a9dcc50e12fe1fd62dce8111a2c
Gilberttherry <gilbertherry333099@gmail.com>
2017-10-25 14:04:51 +0700

    fungsi player 1

--- soal2.c
+++ soal2.c
@@ -4,27 +4,54 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int field1[16];
-int field2[16];
-void *p1(void *arg);
+int temp;
+int field1[17];
+int field2[17];
+int ranjau,count;
+char player1[100];
+char player2[100];
+int stok1=16,stok2=16;
+int nilai1=0,nilai2=0;
+
+void* p1(void *arg)
+{
+	int ranjau;
+	int banyak;
+	while(1)
+	{
+        	printf("pemain 1,Banyak ranjau(max 16) =>  ");
+        	scanf("%d",&banyak);
+        	for(int i=0;i<banyak;i++)
+		{
+			printf("Letakan ranjau di => ");
+	        	scanf("%d",&ranjau);
+            		if(field1[ranjau]==0)stok1--;
+            		field1[ranjau]=1;
+        	}
+        	printf("pemain 1, tebak =>  ");
+		scanf("%d",&ranjau);
+        	if(ladang2[nomor]==0)
+		{
+	            	printf("tebakkan %s benar, poin %s ditambah 1\n",name_1,name_1);
+	            	++skor1;
+	        }
+	        else
+		{
+	        	printf("tebakkan %s salah, poin %s ditambah 1\n",name_1,name_2);
+	         	++skor2;
+        	}
+        	if(skor1==10 || skor2 == 10 || stok1==0 || stok2==0)
+		{
+			return NULL;
+		}
+	}
+    	return NULL;
+}
+
 int main()
 {
-	char player1[100], player2[100];
 	printf("Masukan Nama Player1 => ");
 	scanf("%s",&player1);
 	printf("Masukan Nama Player2 => ");
 	scanf("%s",&player2);
-}
-void *p1(void *arg)
-{
-	int ranjau;	
-	int banyak;
-	printf("Berapa banyak ranjau yang akan dipasang => ");
-	scanf("%d",&banyak);
-	for(int i=0;i<banyak;i++)
-	{
-		printf("Letakan ranjau di => ");
-		scanf("%d",&ranjau);
-		field1[ranjau]=1;
-	}
 }
