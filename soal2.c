f50ef5e5783c03e08238ddfb8cbdb4f8813c4de7
Unknown <rtejakusuma21@yahoo.com>
2017-10-25 12:19:43 +0700

    Fungsi player1

--- soal2.c
+++ soal2.c
@@ -4,6 +4,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+int field1[16];
+int field2[16];
+void *p1(void *arg);
 int main()
 {
 	char player1[100], player2[100];
@@ -11,4 +14,17 @@ int main()
 	scanf("%s",&player1);
 	printf("Masukan Nama Player2 => ");
 	scanf("%s",&player2);
+}
+void *p1(void *arg)
+{
+	int ranjau;	
+	int banyak;
+	printf("Berapa banyak ranjau yang akan dipasang => ");
+	scanf("%d",&banyak);
+	for(int i=0;i<banyak;i++)
+	{
+		printf("Letakan ranjau di => ");
+		scanf("%d",&ranjau);
+		field1[ranjau]=1;
+	}
 }
