bec3aff834227f1d0a7a64e47a6bf3f91708f7f8
Unknown <rtejakusuma21@yahoo.com>
2017-10-25 12:11:44 +0700

    baru inputan nama

---
+++ soal2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main()
+{
+	char player1[100], player2[100];
+	printf("Masukan Nama Player1 => ");
+	scanf("%s",&player1);
+	printf("Masukan Nama Player2 => ");
+	scanf("%s",&player2);
+}
