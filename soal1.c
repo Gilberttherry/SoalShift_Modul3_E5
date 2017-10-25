95d8f5d22548e83e5d503ab7ed0e4ed7831d3a39
Unknown <rtejakusuma21@yahoo.com>
2017-10-25 09:53:47 +0700

    menu buat penjual/pembeli

--- soal1.c
+++ soal1.c
@@ -6,7 +6,8 @@
 #include <sys/shm.h>
 
 char weapon[6] = {"MP4A1", "PM2-V1", "SPR-3", "SS2-V5", "SPG1-V3", "MINE"};
-
+void menu_jual();
+void menu_beli();
 int main()
 {
 	key_t key=1234;
@@ -14,4 +15,14 @@ int main()
 	int shmid=shmget(key,sizeof(int), IPC_CREAT | 0666);
 	value=shmat(shmid,NULL,0);
 
+}
+void menu_jual()
+{
+	printf("1.Tambah Stock Persenjataan\n");
+	printf("2.List Persenjataan\n\n");
+}
+void menu_beli()
+{
+	printf("1.List Persenjataan\n");
+	printf("2.Beli Senjata\n\n");
 }
